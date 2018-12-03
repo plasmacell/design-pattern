179abcfd2db8e1538e632865d8cddc70ff0815af
plasmacell <hy934954360@gmail.com>
2018-12-03 09:26:04 +0800

    add something

--- src/main/java/com/pattern/factory/factorymethod/FactoryMethodTest.java
+++ src/main/java/com/pattern/factory/factorymethod/FactoryMethodTest.java
@@ -1,5 +1,10 @@
 package com.pattern.factory.factorymethod;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.concurrent.*;
+
 public class FactoryMethodTest {
 
     public static void main(String[] args) {
@@ -14,5 +19,35 @@ public class FactoryMethodTest {
         ReaderFactory jpgFactory = new JpgFactory();
         Reader reader2 = jpgFactory.getReader();
         reader2.read();
+
+        myThread();
+        myFunction();
+    }
+
+    private static void myThread() {
+
+        ThreadGroup group = new ThreadGroup("Group");
+        group.list();
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("new thread.");
+            }
+        };
+        thread.start();
+    }
+
+    private static void myFunction() {
+        int[] iArr = {1, 43, 54, 32, 65, 33, 64};
+        Arrays.stream(iArr).forEach(System.out::println);
+    }
+
+    public void print() {
+        if (true) {
+            System.out.println(111);
+        } else {
+            System.out.println("wiee");
+        }
     }
 }
